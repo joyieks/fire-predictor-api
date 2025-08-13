aeffe800f22cacfdc45c419d286913d469b05ff2
kfpriego01 <diocampojoanjoy24@gmail.com>
2025-08-13 21:50:10 +0800

    fix: modified secured cloudinary

--- cloudinary_config.py
+++ cloudinary_config.py
@@ -1,10 +1,12 @@
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 
+# Use environment variables for security
 cloudinary.config(
-    cloud_name="dlnq8eaed",
-    api_key="495443256383188",
-    api_secret="PjKijs6b2Y_vHXSESYbvRwWG2IQ",
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME", "dlnq8eaed"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY", "495443256383188"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),  # Must be set as env var
     secure=True
 )
